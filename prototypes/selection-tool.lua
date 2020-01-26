a773efe042bb13bf663c983ad46020a43a03e6ad
muppet9010 <40836017+muppet9010@users.noreply.github.com>
2020-01-26 16:13:37 +0000

    0.18 update

--- prototypes/selection-tool.lua
+++ prototypes/selection-tool.lua
@@ -8,16 +8,15 @@ data:extend(
             icon = Constants.AssetModName .. "/graphics/item/tape-measure.png",
             icon_size = 640,
             stack_size = 1,
-            stackable = false,
             subgroup = "other",
             order = "a",
+            flags = {"mod-openable", "not-stackable"},
             selection_color = {r = 0, g = 1, b = 0},
             selection_mode = {"any-tile"},
             selection_cursor_box_type = "entity",
             alt_selection_color = {a = 0},
             alt_selection_mode = {"nothing"}, --nothing will highlight
-            alt_selection_cursor_box_type = "entity",
-            can_be_mod_opened = true
+            alt_selection_cursor_box_type = "entity"
         }
     }
 )
